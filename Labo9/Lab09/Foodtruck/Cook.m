21e5060a601875346778f213c213634b862c760f
Javier Jesus  Carreño Reyna <35305955+car251290@users.noreply.github.com>
2018-09-13 12:06:17 -0700

    Add files via upload

---
+++ Labo9/Lab09/Foodtruck/Cook.m
@@ -0,0 +1,15 @@
+//
+//  Cook.m
+//  Foodtruck
+//
+//  Created by Javier Carreno Reyna on 2018-09-13.
+//  Copyright © 2018 Lighthouse Labs. All rights reserved.
+//
+
+#import "Cook.h"
+
+@implementation Cook
+-(double)foodTruck:(FoodTruck *)truck priceForFood:(NSString *)food{
+    return 10.0;
+}
+@end
